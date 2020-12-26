aacbc8357ba7e29cf73f50533128e930088b33c5
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2020-12-25 16:19:51 -0800

    js: added descriptive comment

--- timesheet.js
+++ timesheet.js
@@ -1,6 +1,6 @@
-console.log('starting JavaScript');
+/* Make transparent label above input field slowly become opaque
+when input recieves "focus".  */
 input = document.querySelectorAll("input");
-
 input.forEach( element => {
   element.addEventListener("focus", e => {
     var selector = document.querySelector('[for = "' + e.target.id + '"]');
